88c22191ae6a715fa98eddfbf1c01d641ad5aecc
Nusnan .A.M <82087528+mohamednusnan@users.noreply.github.com>
2024-12-02 19:07:32 +0530

    ns

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      <div>
-        <div>
-          <span>Nus</span>
-          <span>Nan</span>
+      <div className='container mx-auto hidden md:flex justify-between items-center py-6'>
+        <div className='text-xl lg:text-2xl font-bold flex items-center gap-1'>
+          <span className='text-white'>Nus</span>
+          <span className='text-orange-600'>Nan</span>
         </div>
-        <div>
-          <ul>
-            <li>About</li>
-            <li>Service</li>
-            <li>Work</li>
-            <li>Contact</li>
-          </ul>
-        </div>
+        <div className="flex items-center text-white">
+  <ul className="flex space-x-4">
+    <li className="hover:text-gray-300 cursor-pointer">About</li>
+    <li className="hover:text-gray-300 cursor-pointer">Service</li>
+    <li className="hover:text-gray-300 cursor-pointer">Work</li>
+    <li className="hover:text-gray-300 cursor-pointer">Contact</li>
+  </ul>
+</div>
         <a>Download CV</a>
       </div>
     </div>
