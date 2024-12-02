1c2fa9c8565adb925709ad815bb8ca2f92ce540c
Nusnan .A.M <82087528+mohamednusnan@users.noreply.github.com>
2024-12-02 16:56:55 +0530

    nav 1

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <span>Nus</span>
+          <span>Nan</span>
+        </div>
+        <div>
+          <ul>
+            <li>About</li>
+            <li>Service</li>
+            <li>Work</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <a>Download CV</a>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
