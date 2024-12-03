ec57590af4ba3dac91925efa4621ae3309f9db7b
Nusnan .A.M <82087528+mohamednusnan@users.noreply.github.com>
2024-12-03 23:56:07 +0530

    anotherup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
 
 const App = () => {
   return (
     <div className='bg-[#1d0000]'>
      <Navbar/>
+     <Hero/>
     </div>
   )
 }
